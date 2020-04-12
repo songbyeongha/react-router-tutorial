9c6495c10394feeb2e46a8e905aaa0b65639f1f2
songbyeongha <qudgk0006@naver.com>
2020-04-12 18:35:41 +0900

    파라미터와 쿼리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/homer">프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
